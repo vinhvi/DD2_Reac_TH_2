6071d80e15fdb065505e1ef3774f489e805b4910
vinhvi <69356916+vinhvi@users.noreply.github.com>
2022-09-18 00:42:00 +0700

    finish

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image, Button } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.view3}>
+        <Image
+          style={{ marginTop: 50 }}
+          source={require("./image/circle.png")}
+        />
+        <StatusBar style="auto" />
+      </View>
+      <View style={styles.view2}>
+        <Text style={{ fontSize: 59, textAlign: "center", marginTop: 40 }}>
+          GROW YOUR BUSINEE
+        </Text>
+      </View>
+      <View style={{ flex: 1 }}>
+        <Text style={{ fontSize: 18, textAlign: "center" }}>
+          We will help you to grow your business using online server
+        </Text>
+      </View>
+      <View style={styles.action}>
+        <Button color="#E3C000" title="LOGIN" />
+        <Button color="#E3C000" title="SIGN UP" />
+      </View>
+      <View style={{ flex: 1 }}>
+        <Text style={{ marginTop: 30, fontSize: 20 }}>How We Work?</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +34,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#28F7AC",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  view2: {
+    flex: 1,
+    alignItems: "center",
+    textShadowColor: "black",
+    fontSize: 20,
+  },
+  action: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "50%",
+  },
+  button: {
+    backgroundColor: "#E3C000",
   },
 });
